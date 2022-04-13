5f33ef6ac5bf0546ef721038a461e86031d6bdbe
FerVn99 <guilleedusj@gmail.com>
2022-04-13 16:32:20 -0600

    ""

---
+++ Promedio/promedio.js
@@ -0,0 +1,11 @@
+// Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+// Es posible que el codigo generado no sea completamente correcto. Si encuentra
+// errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+function promedio() {
+  var n1, n2, respuesta;
+  n1 = Number(prompt("Inserta el primer numero"));
+  n2 = Number(prompt("Inserta el segundo numero"));
+  respuesta = (n1 + n2) / 2;
+  document.getElementById("Boton").innerHTML = "El promedio es: " + respuesta;
+}
